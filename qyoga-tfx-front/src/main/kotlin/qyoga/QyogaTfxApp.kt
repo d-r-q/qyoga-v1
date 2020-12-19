ef45eb83905e5559722113869315e76dea247387
Aleksey Zhidkov <aleksey.zhidkov@pm.me>
2020-12-19 19:58:22 +0700

    (фронт) реализована максимизация окна для экранов <= 1920 пикселей

--- qyoga-tfx-front/src/main/kotlin/qyoga/QyogaTfxApp.kt
+++ qyoga-tfx-front/src/main/kotlin/qyoga/QyogaTfxApp.kt
@@ -14,10 +14,14 @@ class QyogaTfxApp : App(ExercisesDashboardView::class) {
     override fun start(stage: Stage) {
         super.start(stage)
         FX.dicontainer = DI(initComponents())
-        stage.width = 1920.0
-        stage.height = 1080.0
-        stage.x = (Screen.getPrimary().visualBounds.width - 1920.0) / 2
-        stage.y = (Screen.getPrimary().visualBounds.height - 1080.0) / 2
+        if (Screen.getPrimary().bounds.width <= 1920.0) {
+            stage.isMaximized = true
+        } else {
+            stage.width = 1920.0
+            stage.height = 1080.0
+            stage.x = (Screen.getPrimary().visualBounds.width - 1920.0) / 2
+            stage.y = (Screen.getPrimary().visualBounds.height - 1080.0) / 2
+        }
     }
 
     private fun initComponents(): Set<Any> {
